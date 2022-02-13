aad7a76873fad12db4c902a49e0f27152ea9dfe7
Meka Egwuekwe <meka@code-crew.org>
2022-02-12 23:13:31 -0600

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
+import TodoList from './TodoList'
+//import uuidv4 from 'uuid/v4'
+
+const LOCAL_STORAKE_KEY = 'todoApp.todos'
 
 function App() {
+  // first var is all the todos; second is the function we call to update the todos
+  const [todos, setTodos] = useState( [] ) // [{ id: 1, name: 'Todo 1', complete: false }]
+  const todoNameRef = useRef()
+  
+  useEffect( () => {
+    const storedTodos = JSON.parse( localStorage.getItem( LOCAL_STORAKE_KEY ) )
+    if( storedTodos ) 
+      setTodos( storedTodos )
+  }, [] )
+
+  useEffect( () => {
+    localStorage.setItem( LOCAL_STORAKE_KEY, JSON.stringify( todos ) )
+  }, [todos] )
+
+  function toggleTodo( id ) {
+    const newTodos = [...todos]
+    const todo = newTodos.find( todo => todo.id === id )
+    todo.complete = !todo.complete
+    setTodos( newTodos )
+  }
+
+  function handleAddTodo( e ) {
+    const name = todoNameRef.current.value
+    if( name === '' ) 
+      return
+    //console.log( name )
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: Math.random() /*uuidv4()*/, name: name, complete: false }]
+    })
+    todoNameRef.current.value = null
+  } 
+
+  function handleKeyDown( e ) {
+    if( e.key === 'Enter' ) {
+      handleAddTodo( e )
+    }
+  }
+ 
+  function handleClearTodos() {
+    const newTodos = todos.filter( todo => !todo.complete )
+    setTodos( newTodos )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Meka's Hello World Todo List React App</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input id="todoInput" ref={todoNameRef} type="text" onKeyPress={handleKeyDown}/>
+      <button id="addTodoBtn" onClick={handleAddTodo}>Add Todo</button>
+      <button id="clearTodosBtn" onClick={handleClearTodos}>Clear Completed Todos</button>
+      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+    </>
   );
 }
 
